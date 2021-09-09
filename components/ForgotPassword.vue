e724290fd3b1d6c7465c742274873d063614f33d
gabriel-alonso <dev.gabriel.alonso@gmail.com>
2021-09-08 21:30:12 -0300

    feat: add forgot password

---
+++ components/ForgotPassword.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="flex-box container-box">
+    <div class="content-box">
+      <div class="flex">
+        <img src="@/assets/img/backArrow.png" class="left-arrow" @click="goBack" />
+      </div>
+      <h2 class="h2">Esqueci a Senha</h2>
+      <v-form ref="form" v-model="valid" lazy-validation>
+        <v-text-field
+          v-model="cpf"
+          label="CPF"
+          required
+          :rules="{
+            required: true,
+            digits: 11,
+            regex: '[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}',
+          }"
+        ></v-text-field>
+        <v-btn class="mr-4 btn" type="submit" :disabled="invalid"> Enviar </v-btn>
+      </v-form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    cpf: null,
+  }),
+  methods: {
+    goBack() {
+      this.$store.commit('screen/updateSignin', true);
+      this.$store.commit('screen/updateSignup1', false);
+    },
+    goForward() {
+      this.$store.commit('screen/updateSignup1', false);
+      this.$store.commit('screen/updateSignup2', true);
+    },
+  },
+};
+</script>
---
+++ pages/forgotPassword.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-app>
+    <div class="background">
+      <ForgotPassword />
+    </div>
+  </v-app>
+</template>
+<script>
+export default {}
+</script>
