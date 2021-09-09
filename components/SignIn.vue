3052cc9f6eb7a2d9893747b08180eda0c272e62b
gabriel-alonso <dev.gabriel.alonso@gmail.com>
2021-09-08 22:19:27 -0300

    feat: add nuxt link forgot password

--- components/SignIn.vue
+++ components/SignIn.vue
@@ -2,7 +2,7 @@
   <div class="flex-box container-box">
     <div class="content-box">
       <img src="@/assets/img/logo-aluguei-140x141.png" class="logo" />
-
+      
       <v-form ref="form" v-model="valid" lazy-validation>
         <v-text-field v-model="email" label="E-mail" required></v-text-field>
         <v-text-field
@@ -11,7 +11,9 @@
           label="Senha"
           required
         ></v-text-field>
-        <h4 class="link left">Esqueci a senha</h4>
+         <NuxtLink to="/esqueci-a-senha">
+          <h4 class="link left">Esqueci a senha</h4>
+        </NuxtLink>
         <NuxtLink to="/cadastrar"
           ><h4 class="link right">Cadastrar</h4>
         </NuxtLink>
