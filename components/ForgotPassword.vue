799c472f5e7cc791254569a10d32d2176c9b25f3
gabriel-alonso <dev.gabriel.alonso@gmail.com>
2021-09-13 20:04:34 -0300

    feat: add mask cpf forgot password

--- components/ForgotPassword.vue
+++ components/ForgotPassword.vue
@@ -2,39 +2,30 @@
   <div class="flex-box container-box">
     <div class="content-box">
       <div class="flex">
-        <img src="@/assets/img/backArrow.png" class="left-arrow" @click="goBack" />
+        <NuxtLink to="/">
+          <img src="@/assets/img/backArrow.png" class="left-arrow" />
+        </NuxtLink>
       </div>
       <h2 class="h2">Esqueci a Senha</h2>
       <v-form ref="form" v-model="valid" lazy-validation>
         <v-text-field
           v-model="cpf"
+          v-mask="'###.###.###-##'"
           label="CPF"
           required
-          :rules="{
-            required: true,
-            digits: 11,
-            regex: '[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}',
-          }"
         ></v-text-field>
+        <the-mask :mask="['###.###.###-##', '##.###.###/####-##']" />
         <v-btn class="mr-4 btn" type="submit" :disabled="invalid"> Enviar </v-btn>
       </v-form>
     </div>
   </div>
 </template>
 <script>
+import {mask} from 'vue-the-mask'
 export default {
+  directives: {mask},
   data: () => ({
     cpf: null,
   }),
-  methods: {
-    goBack() {
-      this.$store.commit('screen/updateSignin', true);
-      this.$store.commit('screen/updateSignup1', false);
-    },
-    goForward() {
-      this.$store.commit('screen/updateSignup1', false);
-      this.$store.commit('screen/updateSignup2', true);
-    },
-  },
 };
 </script>
