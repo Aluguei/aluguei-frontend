07eb64561447cd20b3f2bf4738fa4129191d7bc2
gabriel-alonso <dev.gabriel.alonso@gmail.com>
2021-09-19 15:14:03 -0300

    fix: update plugin vuetify

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,11 +1,6 @@
-import '@fortawesome/fontawesome-free/css/all.css' // Ensure you are using css-loader
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 
-Vue.use(Vuetify)
+import 'assets/scss/style.scss'
 
-export default new Vuetify({
-  icons: {
-    iconfont: 'fa',
-  },
-})
+Vue.use(Vuetify)
