48c174b8d92f0da59977a3a6026bf7675ee08335
gabriel-alonso <dev.gabriel.alonso@gmail.com>
2021-09-20 00:10:09 -0300

    feat: add component product advertise and details

---
+++ components/Products/ProductAdvertise.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="d-flex align-center justify-center container-box">
+    <div class="content-box">
+      <h2>Anunciar Produto</h2>
+      <v-form ref="form" v-model="valid" lazy-validation>
+        <v-card class="bs-none py-5 bg-gray mx-8">
+          <v-icon>mdi-attachment</v-icon>
+          <v-file-input prepend-icon clear-icon hide-details hide-input multiple label="+"></v-file-input>
+        </v-card>
+        <v-text-field v-model="name" :rules="nameRules" label="Nome" required></v-text-field>
+        <v-text-field v-model="description" :rules="descriptionRules" label="Descrição" required></v-text-field>
+        <v-btn class="mr-4 btn" type="submit"> Anunciar </v-btn>
+      </v-form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    name: null,
+    description: null,
+    emailConfirm: null,
+  }),
+};
+</script>
---
+++ components/Products/ProductDetails.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-app class="bg-gray">
+    <v-card class="bs-none py-5 bg-gray mx-10">
+      {{ path }}
+    </v-card>
+    <div class="product-details mx-10">
+      <div class="d-flex">
+        <div class="fb-50">
+          <img src="~/assets/img/products/guitar/guitar-01-1.webp" />
+        </div>
+        <div class="fb-50">
+          <h3 class="font-weight-bold color-black">{{ name }}</h3>
+          <h6 class="color-gray">{{ category }}</h6>
+          <h4 class="color-orange mb-4">{{ price }}</h4>
+          <h4 class="font-weight-bold color-black">Descrição</h4>
+          <p class="mb-5">{{ description }}</p>
+          <h4 class="font-weight-bold color-black">Anunciante</h4>
+          <p class="mb-1"><span class="font-weight-bold color-black">Nome: </span> {{ advertiser.name }}</p>
+          <p class="mb-1"><span class="font-weight-bold color-black">Estado: </span>{{ advertiser.state }}</p>
+          <p class="mb-1"><span class="font-weight-bold color-black">Cidade: </span>{{ advertiser.city }}</p>
+          <v-btn class="mr-4 btn">Alugar</v-btn>
+        </div>
+      </div>
+    </div>
+  </v-app>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      path: 'Home >  Produtos >  Instrumentos Musicais > Guitarra Telecaster',
+      name: 'TeleCaster Preta',
+      rating: 0,
+      category: 'Instrumento Musical',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.',
+      price: '$89/hour',
+      advertiser: {
+        name: 'Gabriel Alonso',
+        state: 'São Paulo',
+        city: 'Campinas',
+      },
+    };
+  },
+  methods: {
+    setRating(rating) {
+      this.rating = rating;
+    },
+  },
+};
+</script>
