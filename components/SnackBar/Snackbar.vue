9807a55473d43664cc85cf9fe10d1c9a1135bfc8
gabriel-alonso <dev.gabriel.alonso@gmail.com>
2021-10-12 17:28:58 -0300

    feat: add component snackbar

---
+++ components/SnackBar/Snackbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-snackbar v-model="$props.snackbar" color="error">
+    Erro. Tente Novamente! Não foi possível realizar o acesso.
+    <template #action="{ attrs }">
+      <v-btn text v-bind="attrs" @click="$props.snackbar = false"> Fechar </v-btn>
+    </template>
+  </v-snackbar>
+</template>
+<script>
+export default {
+  props: ["snackbar"],
+};
+</script>
