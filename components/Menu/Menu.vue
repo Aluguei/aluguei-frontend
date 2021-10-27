407e30fe84daca779dee5655b97f20f752d47b7d
gabriel-alonso <dev.gabriel.alonso@gmail.com>
2021-10-27 20:14:37 -0300

    feat: add links menu

--- components/Menu/Menu.vue
+++ components/Menu/Menu.vue
@@ -2,7 +2,7 @@
   <v-card class="d-flex bg-none bs-none" max-width="400" tile>
     <v-list dense class="bg-none">
       <v-list-item-group color="primary" class="d-flex">
-        <v-list-item v-for="(item, i) in items" :key="i">
+        <v-list-item v-for="(item, i) in items" :key="i" :to="item.link" link>
           <v-list-item-content>
             <v-list-item-title
               class="color-white font-weight-medium fs-small"
@@ -20,9 +20,9 @@ export default {
     // selectedItem: 1,
     items: [
       { text: "Home" },
-      { text: "Sobre nós" },
-      { text: "Anunciar produto" },
-      { text: "Contato" },
+      { text: "Sobre nós", link: "sobre-nos" },
+      { text: "Anunciar produto", link: "/anunciar-produto" },
+      { text: "Contato", link: "/contato" },
     ],
   }),
 };
