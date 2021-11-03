4f697d2439ec1bb7c69c9f099fe827f851a736f7
gabriel-alonso <dev.gabriel.alonso@gmail.com>
2021-11-03 19:59:06 -0300

    fix: update form product advertise

--- components/Products/ProductAdvertise.vue
+++ components/Products/ProductAdvertise.vue
@@ -5,23 +5,32 @@
       <v-form @submit.prevent="submitForm(productInfo)">
         <v-text-field v-model="productInfo.name" label="Nome"></v-text-field>
         <v-text-field v-model="productInfo.description" label="Descrição"></v-text-field>
-        <v-text-field v-model="productInfo.price" label="Preço"></v-text-field>
         <v-text-field v-model="productInfo.category" label="Categoria"></v-text-field>
-        <v-combobox
-          v-model="productInfo.timeUnit"
-          clearable
-          hide-selected
-          persistent-hint
-          :items="items"
-        ></v-combobox>
-        <v-text-field v-model="productInfo.timeQuantity" label="Valor"></v-text-field>
+        <div class="d-flex">
+          <v-text-field
+            v-model="productInfo.price"
+            v-mask="'R$ ####,##'"
+            label="Valor"
+          ></v-text-field>
+          <p>Por</p>
+          <v-combobox
+            v-model="productInfo.timeUnit"
+            clearable
+            hide-selected
+            persistent-hint
+            :items="items"
+            label="Tempo"
+          ></v-combobox>
+        </div>
         <v-btn class="mr-4 btn mt-10 color-white" type="submit"> Anunciar </v-btn>
       </v-form>
     </div>
   </div>
 </template>
 <script>
+import { mask } from "vue-the-mask";
 export default {
+  directives: { mask },
   data: () => ({
     productInfo: {
       name: null,
@@ -31,12 +40,10 @@ export default {
       timeUnit: null,
       timeQuantity: null,
     },
-    // items: ["Por Hora", "Por Dia", "Por Semana"],
-    items: [1, 2, 3],
+    items: ["Por Hora", "Por Dia", "Por Semana"],
   }),
   methods: {
     async submitForm(formInfo) {
-      alert(JSON.stringify(formInfo));
       try {
         const config = {
           headers: {
@@ -56,7 +63,6 @@ export default {
           },
           config
         );
-
         console.log(response);
         return { response };
       } catch (error) {
