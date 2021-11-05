f94f9e2868646c937b0efb885182a23a584d5eb9
gabriel-alonso <dev.gabriel.alonso@gmail.com>
2021-11-04 23:28:38 -0300

    feat: update form search bar

--- components/Search/Search.vue
+++ components/Search/Search.vue
@@ -1,22 +1,24 @@
 <template>
-  <div class="search bg-white d-flex">
-    <v-autocomplete
-      v-model="select"
-      append-icon=""
-      :loading="loading"
-      :items="items"
-      :search-input.sync="search"
-      cache-items
-      class="mx-4"
-      flat
-      hide-no-data
-      hide-details
-      label="Buscar produtos, marcas e muito mais..."
-      solo
-    ></v-autocomplete>
-    <v-btn class="bg-blue-medium px-7 py-1 ma-1 rounded-xl height-auto" type="submit">
-      <h6><v-icon class="color-white">mdi-magnify</v-icon></h6>
-    </v-btn>
+  <div class="search bg-white">
+    <v-form class="d-flex" @submit.prevent="submitForm(search)">
+      <v-autocomplete
+        v-model="select"
+        append-icon=""
+        :loading="loading"
+        :items="items"
+        :search-input.sync="search"
+        cache-items
+        class="mx-4"
+        flat
+        hide-no-data
+        hide-details
+        label="Buscar produtos, marcas e muito mais..."
+        solo
+      ></v-autocomplete>
+      <v-btn class="bg-blue-medium px-7 py-1 ma-1 rounded-xl height-auto" type="submit">
+        <h6><v-icon class="color-white">mdi-magnify</v-icon></h6>
+      </v-btn>
+    </v-form>
   </div>
 </template>
 <script>
@@ -25,6 +27,7 @@ export default {
     return {
       loading: false,
       items: [],
+      ids: [],
       search: null,
       select: null,
       products: [],
@@ -43,7 +46,7 @@ export default {
       this.loading = true;
       setTimeout(() => {
         this.items = this.products.filter((e) => {
-          return (e || '').toLowerCase().includes((v || '').toLowerCase());
+          return (e || "").toLowerCase().includes((v || "").toLowerCase());
         });
         this.loading = false;
       }, 500);
@@ -52,17 +55,27 @@ export default {
       try {
         const config = {
           headers: {
-            device: 'mobile',
+            device: "mobile",
           },
         };
-        const products = await this.$axios.$get('/api/products/available', config);
+        const products = await this.$axios.$get("/api/products/available", config);
         products.data.map((product) => this.items.push(product.name));
+        products.data.map((product) => this.ids.push(product.id));
 
         console.log(products);
         return { products };
       } catch (error) {
         console.log(error);
       }
+    },
+    submitForm(productName) {
+      let productId;
+      this.items.forEach((element, index) => {
+        if (element === productName) {
+          productId = this.ids[index];
+        }
+      });
+      this.$router.push(`/detalhes-produto/${productId}`);
     },
   },
 };
