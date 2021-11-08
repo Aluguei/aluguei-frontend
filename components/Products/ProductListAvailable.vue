cc618ed21c56a69215186a1ed89d42758f425efe
gabriel-alonso <dev.gabriel.alonso@gmail.com>
2021-11-08 02:18:02 -0300

    feat: add loading list products

--- components/Products/ProductListAvailable.vue
+++ components/Products/ProductListAvailable.vue
@@ -1,7 +1,7 @@
 <template>
   <v-sheet class="rounded ma-8">
-    <h3 class="mb-6 pa-7">Lista de Produtos</h3>
-    <v-slide-group show-arrows>
+    <h3 class="pa-7">Lista de Produtos</h3>
+    <v-slide-group v-if="!loading" show-arrows>
       <v-slide-item v-for="(item, i) in items" :key="i">
         <v-list>
           <v-list-item-group>
@@ -22,14 +22,24 @@
         </v-list>
       </v-slide-item>
     </v-slide-group>
+    <div v-else-if="loading" class="d-flex align-center justify-center">
+      <div style="text-align: center">
+        <h4 class="color-gray mb-3">Carregando os produtos...</h4>
+        <img src="@/assets/img/logo/loading-logo.gif" />
+      </div>
+    </div>
   </v-sheet>
 </template>
 <script>
+/* eslint-disable vue/no-unused-components */
+import Loading from "../Loading/Loading.vue";
 export default {
+  components: { Loading },
   data: () => ({
     model: null,
     items: [],
     meta: [],
+    loading: true,
   }),
   mounted() {
     this.listProdutcts();
@@ -42,15 +52,19 @@ export default {
             device: "mobile",
           },
         };
-        const products = await this.$axios.$get("/api/products/available", config);
+        const products = await this.$axios
+          .$get("/api/products/available", config)
+          .finally(() => {
+            this.loading = false;
+          });
 
         products.data.map((product) => this.items.push(product));
         // products.meta.map((product) => this.items.push(product));
 
-        console.log(products);
+        // console.log(products);
         return { products };
       } catch (error) {
-        console.log(error);
+        // console.log(error);
       }
     },
   },
