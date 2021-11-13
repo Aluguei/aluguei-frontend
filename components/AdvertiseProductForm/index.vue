44edb7679f9931040b02980da3d6f69199e6edd1
gabriel-alonso <dev.gabriel.alonso@gmail.com>
2021-11-13 17:16:23 -0300

    feat: change form advertise

--- components/AdvertiseProductForm/index.vue
+++ components/AdvertiseProductForm/index.vue
@@ -3,14 +3,20 @@
     <v-form @submit.prevent="submitForm(productInfo)">
       <v-text-field v-model="productInfo.name" label="Nome" />
       <v-text-field v-model="productInfo.description" label="Descrição" />
-      <v-text-field v-model="productInfo.category" label="Categoria" />
+      <v-select
+        v-model="productInfo.category"
+        item-value="value"
+        item-text="label"
+        :items="itemsCetegory"
+        label="Categoria"
+      />
       <v-text-field v-model="productInfo.price" label="Valor" />
       <v-text-field v-model="productInfo.timeQuantity" label="Tempo" />
       <v-select
         v-model="productInfo.timeUnit"
         item-value="value"
         item-text="label"
-        :items="items"
+        :items="itemsTimeUnit"
         label="Por"
       />
       <v-btn class="mr-4 btn mt-10 color-white" type="submit">Anunciar</v-btn>
@@ -19,6 +25,8 @@
 </template>
 <script>
 import { mask } from "vue-the-mask";
+import { mapActions, mapGetters } from "vuex";
+
 export default {
   directives: { mask },
   data: () => ({
@@ -30,38 +38,31 @@ export default {
       timeUnit: null,
       timeQuantity: null,
     },
-    items: [
+    itemsTimeUnit: [
       { label: "Hora", value: "hourly" },
       { label: "Dia", value: "daily" },
       { label: "Semana", value: "weekly" },
     ],
+    itemsCetegory: [
+      { label: "Veiculos", value: "vehicle" },
+      { label: "Tecnologia", value: "technology" },
+      { label: "Ferramenta", value: "tools" },
+      { label: "Esportes", value: "sports" },
+      { label: "Moda", value: "fashion" },
+    ],
   }),
+  computed: {
+    ...mapGetters({
+      isOwnedProductsLoading: "products/getIsOwnedProductsLoading",
+    }),
+  },
+  async mounted() {
+    this.products = await this.getOwnedProducts();
+  },
   methods: {
-    async submitForm(formInfo) {
-      try {
-        const config = {
-          headers: {
-            device: "mobile",
-          },
-        };
-
-        const response = await this.$axios.$post(
-          "/api/products",
-          {
-            name: formInfo.name,
-            description: formInfo.description,
-            category: formInfo.category,
-            price: parseInt(formInfo.price),
-            timeUnit: formInfo.timeUnit,
-            timeQuantity: parseInt(formInfo.timeQuantity),
-          },
-          config
-        );
-        return { response };
-      } catch (error) {
-        console.log(error);
-      }
-    },
+    ...mapActions({
+      getOwnedProducts: "products/getOwnedProducts",
+    }),
   },
 };
 </script>
