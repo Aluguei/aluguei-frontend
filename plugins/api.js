c30d21151adf2ecbd171f0e0483f299af4598fc4
gabriel-alonso <dev.gabriel.alonso@gmail.com>
2021-11-14 17:26:17 -0300

    feat: update api plugin and store dialog and products

--- plugins/api.js
+++ plugins/api.js
@@ -1,41 +1,41 @@
-const axios = require('axios')
+const axios = require('axios');
 
 export default class Api {
   static http = axios.create({
     baseURL: 'https://aluguei-backend.herokuapp.com/api',
     headers: {
-      device: 'mobile'
-    }
-  })
+      device: 'mobile',
+    },
+  });
 
   static removeEmptyValues(q) {
-    return Object.fromEntries(Object.entries(q).filter(([_, v]) => !!v))
+    return Object.fromEntries(Object.entries(q).filter(([_, v]) => !!v));
   }
 
   static async _call({ path, method = 'get', headers = {}, query = {} }) {
-    const authorization = window.localStorage.getItem('auth._token.local')
+    const authorization = window.localStorage.getItem('auth._token.local');
 
-    const sanatizedHeaders = this.removeEmptyValues(headers)
-    const sanatizedQuery = this.removeEmptyValues(query)
+    const sanatizedHeaders = this.removeEmptyValues(headers);
+    const sanatizedQuery = this.removeEmptyValues(query);
 
     return await this.http[method](path, {
       headers: { ...sanatizedHeaders, authorization },
-      params: sanatizedQuery
-    })
+      params: sanatizedQuery,
+    });
   }
 
   static async getAvailableProducts(query = {}) {
-    const { data } = await this._call({ path: '/products/available', query })
-    return data
+    const { data } = await this._call({ path: '/products/available', query });
+    return data;
   }
 
-  static async getOwnedProducts() {
-    const { data } = await this._call({ path: '/products/owned' })
-    return data
+  static async getOwnedProducts(query = {}) {
+    const { data } = await this._call({ path: '/products/owned', query });
+    return data;
   }
 
   static async setOwnedProducts() {
-    const { data } = await this._call({ path: '/products/owned' })
-    return data
+    const { data } = await this._call({ path: '/products/owned' });
+    return data;
   }
 }
--- store/products/mutations.js
+++ store/products/mutations.js
@@ -1,17 +1,22 @@
 export default {
-  setAvailableProducts(state, availableProducts = []) {
-    Object.assign(state, { availableProducts })
-  },
-
-  setProductsSearch(state, productsSearch = {}) {
-    Object.assign(state, { productsSearch })
-  },
-
-  setIsAvailableProductsLoading(state, isAvailableProductsLoading = false) {
-    Object.assign(state, { isAvailableProductsLoading })
-  },
-
-  setOwnedProducts(state, products = []) {
-    Object.assign(state, { products })
-  }
-}
+    setAvailableProducts(state, availableProducts = []) {
+      Object.assign(state, { availableProducts });
+    },
+  
+    setOwnedProducts(state, products = []) {
+      Object.assign(state, { products });
+    },
+  
+    setProductsSearch(state, productsSearch = {}) {
+      Object.assign(state, { productsSearch });
+    },
+  
+    setIsAvailableProductsLoading(state, isAvailableProductsLoading = false) {
+      Object.assign(state, { isAvailableProductsLoading });
+    },
+  
+    setIsOwnedProductsLoading(state, isOwnedProductsLoading = false) {
+      Object.assign(state, { isOwnedProductsLoading });
+    },
+  };
+  
