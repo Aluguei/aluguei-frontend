1dee07d0a8233fff6972e6ceaf44da5fecd34ba8
gabriel-alonso <dev.gabriel.alonso@gmail.com>
2021-11-17 21:22:18 -0300

    fix: rollback loading forgot password

---
+++ components/Waiting/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="d-flex align-center justify-center">
+    <div class="overlay">
+      <img class="loadingImage" src="@/assets/img/logo/loading-logo.gif" />
+    </div>
+  </div>
+</template>
