4cb01bdba19d9488aeec27ecc962443a65d21f61
gabriel-alonso <dev.gabriel.alonso@gmail.com>
2021-11-20 16:34:36 -0300

    feat: add faq page

--- components/FAQ/index.vue
+++ components/FAQ/index.vue
@@ -4,12 +4,12 @@
       <h2 class="text-center color-black">Dúvidas Frequentes</h2>
       <v-expansion-panels v-model="panel" multiple>
         <v-expansion-panel v-for="(item, i) in items" :key="i">
-          <v-expansion-panel-header>Header {{ item }}</v-expansion-panel-header>
-          <v-expansion-panel-content>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
+          <v-expansion-panel-header
+            style="background-color: #3385d9; color: white; font-size: 17px"
+            >{{ item.question }}</v-expansion-panel-header
+          >
+          <v-expansion-panel-content style="margin-top: 15px">
+            {{ item.answer }}
           </v-expansion-panel-content>
         </v-expansion-panel>
       </v-expansion-panels>
@@ -22,7 +22,33 @@ export default {
   data() {
     return {
       panel: [],
-      items: 5
+      items: [
+        {
+          question: 'O que é o Aluguei e o que oferece?',
+          answer:
+            'O Projeto Aluguei é uma plataforma capaz de gerenciar negociações de aluguel de produtos entre locatários e clientes. Para que essa demanda seja atendida, desenvolvemos um projeto na versão Web e Mobile e temos como missão conectar pessoas que possuem objetos das quais não utilizam mais ou então que são utilizados com pouca frequência, assim ajudamos pessoas que tem interesse em alugar esses bens para utilização momentânea, levando ao proprietário do produto uma renda extra adquirindo através dos seus equipamentos e também para quem tem interesse em utilizar por um periodo de tempo determinado. Com essa idéia acreditamos que conseguimos contribuir muito com milhares de pessoas.'
+        },
+        {
+          question: 'Como consigo entrar em contato com o Anunciante?',
+          answer:
+            'Para entrar em contato com anunciante, no detalhe do produto que estiver interesse estará aparecendo o telefone da pessoa que criou o anúncio.'
+        },
+        {
+          question: 'Como denunciar um anúncio?',
+          answer:
+            'Para denunciar um anúncio você irá precisar entrar em contato com nossa equipe técnica e explicar o motivo da denûncia para tomarmos a ação da situação.'
+        },
+        {
+          question: 'Em caso de devolução, como funciona?',
+          answer:
+            'Para devolução, você entrará em contato com anunciante para marcar como deseja devolver.'
+        },
+        {
+          question: 'Como eu fecho minha conta?',
+          answer:
+            'Para fechar sua contavocê irá precisar entrar em contato com nossa equipe técnica que realizaremos o encerramento da sua conta.'
+        }
+      ]
     }
   },
   methods: {
@@ -35,3 +61,8 @@ export default {
   }
 }
 </script>
+<style>
+.v-expansion-panel-header__icon i {
+  color: white !important;
+}
+</style>
