c7eb36300fd05c66c34628a3e2581a2cfc9dfe8c
gabriel-alonso <dev.gabriel.alonso@gmail.com>
2021-11-24 20:16:08 -0300

    fix: add messagem sucess forgot password

--- pages/forgotPassword.vue
+++ pages/forgotPassword.vue
@@ -6,10 +6,20 @@
     <div v-else>
       <Waiting />
     </div>
-    <v-snackbar v-model="snackbar" color="error">
+    <v-snackbar v-model="snackbarErro" color="error">
       Erro. Tente Novamente! Esse CPF não foi encontrado.
       <template #action="{ attrs }">
-        <v-btn text v-bind="attrs" @click="snackbar = false"> Fechar </v-btn>
+        <v-btn text v-bind="attrs" @click="snackbarErro = false">
+          Fechar
+        </v-btn>
+      </template>
+    </v-snackbar>
+    <v-snackbar v-model="snackbarSucesso" color="success">
+      Senha recuperada com sucesso!
+      <template #action="{ attrs }">
+        <v-btn text v-bind="attrs" @click="snackbarSucesso = false">
+          Fechar
+        </v-btn>
       </template>
     </v-snackbar>
   </div>
@@ -18,7 +28,8 @@
 export default {
   data() {
     return {
-      snackbar: false,
+      snackbarErro: false,
+      snackbarSucesso: false,
       spinner: false
     }
   },
@@ -35,11 +46,15 @@ export default {
           )
           .finally(() => {
             this.spinner = false
+            this.snackbarSucesso = false
+          })
+          .then(() => {
+            this.snackbarSucesso = true
           })
         return { response }
       } catch (error) {
         console.log(error)
-        this.snackbar = true
+        this.snackbarErro = true
         this.spinner = false
       }
     }
